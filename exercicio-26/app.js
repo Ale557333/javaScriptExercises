3848b8c134b77d2a7a5def33d4a3e0d043265577
Alexandre Oliveira <alexandre.yukon@gmail.com>
2023-08-07 10:21:30 -0400

    Exercises file 26

--- exercicio-26/app.js
+++ exercicio-26/app.js
@@ -5,14 +5,62 @@
     formatação "DD/MM/AAAA". Exemplo: 03/07/2021;
   - Não utilize a date-fns.
 */
+const present = new Date()
 
+const formatTimeUnit = unit => String(unit).length === 1 ? `0${unit}` : unit
+
+const formatDate = date => {
+  const day = formatTimeUnit(date.getDate())
+  const month = formatTimeUnit(date.getMonth() + 1)
+  const year = date.getFullYear()
+  
+  return `${day}/${month}/${year}`
+}
+
+console.log(formatDate(present))
 /*
   02
 
   - Crie uma função que recebe uma data por parâmetro e retorna o horário e a 
     data na formatação: "03:07 - domingo, 7 de junho de 2020";
   - Não utilize a date-fns.
-*/
+  */
+  const weekDays = [
+    'domingo', 
+    'segunda-feira', 
+    'terça-feira', 
+    'quarta-feira', 
+    'quinta-feira', 
+    'sexta-feira', 
+    'sábado'
+  ]
+
+  const monthNames = [
+    'janeiro', 
+    'fevereiro', 
+    'março', 
+    'abril', 
+    'maio', 
+    'junho', 
+    'julho', 
+    'agosto', 
+    'setembro', 
+    'outubro', 
+    'novembro', 
+    'dezembro'
+  ]
+
+const formatDateInfo = date => {
+  const hours = formatTimeUnit(date.getHours())
+  const minutes = formatTimeUnit(date.getMinutes())
+  const weekDay = weekDays[date.getDay()]
+  const monthDay = date.getMonth()
+  const month = monthNames[date.getMonth()]
+  const year = date.getFullYear()
+  return `${hours}:${minutes} - ${weekDay}, ${monthDay} de ${month} de ${year}`
+}
+
+console.log(formatDateInfo(present))
 
 /*
   03
@@ -23,6 +71,10 @@
 */
 
 const user = { id: 42, isVerified: true }
+
+const { id, isVerified } = user
+
+console.log(id, isVerified)
 
 /*
   04
@@ -37,6 +89,11 @@ const user = { id: 42, isVerified: true }
 const robotA = { name: 'Bender' }
 const robotB = { name: 'HAL 9000' }
 
+const { name: nameA } = robotA
+const { name: nameB } = robotB
+
+console.log(robotA, robotB)
+
 /*
   05
 
@@ -50,6 +107,8 @@ const a = 'a'
 const b = 'b'
 const c = 'c'
 
+const alphabet = { a, b, c }
+console.log(alphabet)
 /*
   06
 
@@ -60,20 +119,13 @@ const useDataSomewhereElse = value => {
   console.log(value)
 }
 
-const updateSomething = (data = {}) => {
-  const target = data.target
-  const property = data.property
-  let willChange = data.willChange
+const updateSomething = ({ target, property, willChange } = {}) => {
 
   if (willChange === 'valor indesejado') {
     willChange = 'valor desejado'
   }
 
-  useDataSomewhereElse({
-    target: target,
-    property: property,
-    willChange: willChange
-  })
+  useDataSomewhereElse({ target, property, willChange })
 }
 
 updateSomething({ target: '1', property: '2', willChange: 'valor indesejado' })
@@ -87,19 +139,20 @@ updateSomething({ target: '1', property: '2', willChange: 'valor indesejado' })
 
 const clockContainer = document.querySelector('.clock-container')
 
+const getClockHTML = (hours, minutes, seconds) => `
+<span>${hours}</span> :
+<span>${minutes}</span> :
+<span>${seconds}</span>
+`
+
 const updateClock = () => {
   const present = new Date()
-  const hours = present.getHours()
-  const minutes = present.getMinutes()
-  const seconds = present.getSeconds()
+  const hours = formatTimeUnit(present.getHours())
+  const minutes = formatTimeUnit(present.getMinutes())
+  const seconds = formatTimeUnit(present.getSeconds())
 
-  const clockHTML = `
-    <span>${String(hours).length === 1 ? `0${hours}` : hours}</span> :
-    <span>${String(minutes).length === 1 ? `0${minutes}` : minutes}</span> :
-    <span>${String(seconds).length === 1 ? `0${seconds}` : seconds}</span>
-  `
+  clockContainer.innerHTML = getClockHTML(hours, minutes, seconds)
 
-  clockContainer.innerHTML = clockHTML
 }
 
 setInterval(updateClock, 1000)
